26cf14589f958e55568422584696036cbe8ff6ff
Brent Jackson <jxnblk@gmail.com>
2019-03-13 22:40:52 -0400

    Edit readme

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-// theming
+// core
 export { Provider } from './Provider'
+export { Block } from './Block'
+export { Box } from './Box'
 
-// blocks
+// layout blocks
 export { Bar, bar } from './Bar'
 export { Banner, banner } from './Banner'
-export { Tiles, tiles } from './Tiles'
 export { Cards, cards } from './Cards'
+export { Center, center } from './Center'
 export { Columns, columns } from './Columns'
 export { Split, split } from './Split'
-export { Center, center } from './Center'
+export { Tiles, tiles } from './Tiles'
+// export { MediaObjects, mediaObjects } from './MediaObjects'
 
 // util
-export { Box } from './Box'
 export * from './util'
