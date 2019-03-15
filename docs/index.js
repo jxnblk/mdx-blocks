8e72865b1ca557bee046c1d1f6e39b4ff14a5a68
Brent Jackson <jxnblk@gmail.com>
2019-03-15 19:14:08 -0400

    Add react-helmet

--- docs/index.js
+++ docs/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
+import { Helmet } from 'react-helmet'
 import { BlocksProvider } from '..'
+import pkg from '../package.json'
 import Header from './header.mdx'
 import Banner from './banner.mdx'
 import Features from './features.mdx'
@@ -11,23 +13,21 @@ import Footer from './footer.mdx'
 
 import { funk as theme } from '../themes'
 
-/*
-  <Helmet>
-    <title>MDX Blocks</title>
-    <meta
-      name='description'
-      content={pkg.description}
-    />
-    <link
-      rel='stylesheet'
-      href='https://fonts.googleapis.com/css?family=Poppins400:700:900|Roboto+Mono|Roboto400:600:700'
-    />
-  </Helmet>
-*/
 
 export default props =>
   <BlocksProvider
     {...theme}>
+    <Helmet>
+      <title>MDX Blocks</title>
+      <meta
+        name='description'
+        content={pkg.description}
+      />
+      <link
+        rel='stylesheet'
+        href='https://fonts.googleapis.com/css?family=Poppins400:700:900|Roboto+Mono|Roboto400:600:700'
+      />
+    </Helmet>
     <Header />
     <Banner />
     <Features />
